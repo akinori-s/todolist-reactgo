1679830384e0d7298d8486f4255215eb0a03d890
asasada <terrywu50@hotmail.com>
2023-08-05 23:17:15 +0900

    replaced default page with todo frontend using tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [todos, setTodos] = useState([
+		{ text: "Tasdddddddddddddddddddddddddddddssssssssssss dddddddddddsk 1", completed: false },
+		{ text: "Task 2", completed: false },
+	]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const toggleCompletion = (index) => {
+		const updatedTodos = [...todos];
+		updatedTodos[index].completed = !updatedTodos[index].completed;
+		setTodos(updatedTodos);
+	};
+
+	return (
+		<div className="flex flex-col items-center h-screen w-full py-20 space-y-8 min-w-[300px]">
+      <h1 className="text-2xl text-center">TodoList App</h1>
+			<div className="flex flex-row items-center space-x-4">
+				<input type="text" className="border rounded-md px-2 py-1 w-3/4" placeholder="Enter a task" />
+				<button className="border rounded-md px-2 py-1 bg-blue-500 text-white hover:bg-blue-600">Add</button>
+			</div>
+      <ul className="list-decimal list-inside h-1/2 w-1/2 min-w-[300px] max-w-[500px] overflow-y-auto border rounded-md">
+        {todos.map((todo, index) => (
+          <li key={index} className="flex justify-between items-center py-2 px-4 border-b last:border-0 hover:bg-gray-50 w-full">
+						<div className="flex items-center flex-grow">
+							<button 
+								className={`mr-4 p-[5px] ${todo.completed ? 'bg-green-500' : 'bg-red-500'} rounded`} 
+								onClick={() => toggleCompletion(index)}
+							/>
+							<span className={`${todo.completed ? 'line-through' : ''} break-all whitespace-normal`}>{todo.text}</span>
+  					</div>
+						<button className="px-2 py-1 bg-gray-200 text-white rounded hover:bg-red-400">
+							Delete
+						</button>
+					</li>
+        ))}
+      </ul>
+    </div>
+	);
 }
 
 export default App
