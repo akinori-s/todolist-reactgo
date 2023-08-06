fcd195a5042049ac986f506f3f18d004d5f6b6f8
akinori-s <sasada.a30@gmail.com>
2023-08-06 16:43:36 +0900

    minor: formatting fixes, removed unnecessary console logs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -35,13 +35,13 @@ function App() {
 	};
 
 	const handleDeleteTodo = async (id) => {
-    try {
-      await deleteTodo(id);
-      setTodos(todos.filter(todo => todo.id !== id));
-    } catch (error) {
-      console.error('Failed to delete todo:', error);
-    }
-  };
+		try {
+			await deleteTodo(id);
+			setTodos(todos.filter(todo => todo.id !== id));
+		} catch (error) {
+			console.error('Failed to delete todo:', error);
+		}
+	};
 
 	const handleUpdateTodo = async (id, updatedValues) => {
 		try {
@@ -61,33 +61,32 @@ function App() {
 				const updatedTodo = { ...todo, completed: !todo.completed };
 				handleUpdateTodo(index, updatedTodo);
 		}
-		console.log(todos);
 	};
 
 	return (
 		<div className="flex flex-col items-center h-screen w-full py-20 space-y-8 min-w-[300px]">
-      <h1 className="text-2xl text-center">TodoList App</h1>
+			<h1 className="text-2xl text-center">TodoList App</h1>
 			<div className="flex flex-row items-center space-x-4">
 				<input type="text" className="border rounded-md px-2 py-1 w-3/4" placeholder="Enter a task" value={newTodo} onChange={e => setNewTodo(e.target.value)}/>
 				<button className="border rounded-md px-2 py-1 bg-blue-500 text-white hover:bg-blue-600" onClick={handleAddTodo}>Add</button>
 			</div>
-      <ul className="list-decimal list-inside h-1/2 w-1/2 min-w-[300px] max-w-[500px] overflow-y-auto border rounded-md">
-        {todos.map((todo) => (
-          <li key={todo.id} className="flex justify-between items-center py-2 px-4 border-b last:border-0 hover:bg-gray-50 w-full">
+			<ul className="list-decimal list-inside h-1/2 w-1/2 min-w-[300px] max-w-[500px] overflow-y-auto border rounded-md">
+				{todos.map((todo) => (
+					<li key={todo.id} className="flex justify-between items-center py-2 px-4 border-b last:border-0 hover:bg-gray-50 w-full">
 						<div className="flex items-center flex-grow">
 							<button 
 								className={`mr-4 p-[5px] ${todo.completed ? 'bg-green-500' : 'bg-red-500'} rounded`} 
 								onClick={() => toggleCompletion(todo.id)}
 							/>
 							<span className={`${todo.completed ? 'line-through' : ''} break-all whitespace-normal`}>{todo.text}</span>
-  					</div>
+						</div>
 						<button className="px-2 py-1 bg-gray-200 text-white rounded hover:bg-red-400" onClick={() => handleDeleteTodo(todo.id)}>
 							Delete
 						</button>
 					</li>
-        ))}
-      </ul>
-    </div>
+				))}
+			</ul>
+		</div>
 	);
 }
 
