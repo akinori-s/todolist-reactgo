e5c98d2c71e537cfe9891333279d05878c601504
akinori-s <sasada.a30@gmail.com>
2023-08-07 21:50:20 +0900

    containerised the frontend, backend and db

--- backend/database/db.go
+++ backend/database/db.go
@@ -2,23 +2,17 @@ package database
 
 import (
 	"fmt"
-	// "os"
+	"os"
 	"database/sql"
 	_ "github.com/lib/pq"
 )
 
 func InitDB() (*sql.DB, error) {
-	// host := os.Getenv("DB_HOST")
-	// port := os.Getenv("DB_PORT")
-	// user := os.Getenv("DB_USER")
-	// password := os.Getenv("DB_PASSWORD")
-	// dbname := os.Getenv("DB_NAME")
-	// wip: currently running on local db
-	host := "localhost"
-	port := "5432"
-	user := "golang_usr"
-	password := "golang_pass"
-	dbname := "golang_db"
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
 
 	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
