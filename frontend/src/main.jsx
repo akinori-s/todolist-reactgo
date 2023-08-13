ba76282ad3849424b3489f36670847b48a8e9826
akinori-s <sasada.a30@gmail.com>
2023-08-13 21:35:38 +0900

    added context for authentication

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,18 +6,21 @@ import Header from './components/Header.jsx'
 import LoginPage from './components/LoginPage.jsx'
 import SignUpPage from './components/SignUpPage.jsx'
 import ErrorPage from './components/ErrorPage.jsx'
+import { AuthProvider } from './contexts/AuthContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 	<BrowserRouter>
-		<Header />
-		<Routes>
-			<Route path="/" element={<App />} />
-			<Route path="/login" element={<LoginPage />} />
-			<Route path="/signup" element={<SignUpPage />} />
-			<Route path="*" element={<ErrorPage />} />
-		</Routes>
+		<AuthProvider>
+			<Header />
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/signup" element={<SignUpPage />} />
+				<Route path="*" element={<ErrorPage />} />
+			</Routes>
+		</AuthProvider>
 	</BrowserRouter>
   </React.StrictMode>,
 )
